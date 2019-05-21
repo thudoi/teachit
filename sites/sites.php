34895e4b5db4801b5264c186b05e2c2adb50e43a
Thu Doi <thu.doi@keyconsulting.fr>
2019-05-21 21:44:17 +0700

    update views

--- sites/sites.php
+++ sites/sites.php
@@ -55,7 +55,7 @@
  */
 
 $sites = array(
-  'teachit.test' => 'arianna',
+  'teachit.test' => 'spencer',
   'spencer.teachit.media' => 'spencer',
   'johnwspencer.pro' => 'spencer',
   'johnwspencer.online' => 'spencer',
